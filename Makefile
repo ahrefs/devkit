2c56a0e8b796222b00d2e937893968f753328942
ip <ip@sigxcpu.com>
2009-09-15 07:36:00 +0000

    +TimeLimited2

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ build:
 		ocamlbuild -cflags -I,+extlib -j 0 devkit.cma devkit.cmxa
 
 doc:
-		ocamlbuild devkit.docdir/index.html
+		ocamlbuild -cflags -I,+extlib -j 0 devkit.docdir/index.html
 
 clean:
 		ocamlbuild -clean
--- cache.ml
+++ cache.ml
@@ -3,6 +3,7 @@
 open Prelude
 open Control
 
+(** Thread safe *)
 module TimeLimited(E: sig type t end) = struct
 
   type key = Int64.t
@@ -42,4 +43,54 @@ module TimeLimited(E: sig type t end) = struct
 
 end
 
+module LockNone = struct
+  type t = unit
+  let create () = ()
+  let locked () f = f ()
+end
+
+module LockMutex = struct
+  type t = Mutex.t
+  let create = Mutex.create
+  let locked = Control.locked
+end
+
+module TimeLimited2(E: Set.OrderedType)
+  (Lock: sig type t val create : unit -> t val locked : t -> (unit -> 'a) -> 'a end) = struct
+
+  type time = Int64.t
+
+  let fixed f = 10000. *. f >> Int64.of_float
+  let current () = Unix.gettimeofday () >> fixed
+
+  module Value = struct
+    type t = E.t * time
+    let compare (v1,_) (v2,_) = E.compare v1 v2
+  end
+
+  module M = Set.Make(Value)
+
+  type t = { limit : time; mutable next : time; lock : Lock.t; mutable m : M.t; }
+
+  let private_purge t =
+    let cur = current () in
+    if cur >= t.next then
+    begin
+      t.next <- Int64.add t.limit cur;
+      t.m <- M.filter (fun (_,t) -> t > cur) t.m
+    end
+
+  let create limit = { limit = fixed limit; next = 0L; lock = Lock.create (); m = M.empty }
+ 
+  let add t x =
+    let expire = Int64.add t.limit (current ()) in
+    (* FIXME replace not add *)
+    Lock.locked t.lock (fun () -> private_purge t; t.m <- M.add (x, expire) t.m)
+
+  let get t v =
+    (* lock is not needed *)
+    let found = Lock.locked t.lock (fun () -> M.filter (fun (x,_) -> 0 = E.compare x v) t.m) in
+    try M.choose found >> snd >> some with Not_found -> None
+
+end
 
