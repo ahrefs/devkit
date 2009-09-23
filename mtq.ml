6f91a41c7ce37cfc5c7593fdab98a03fee06fce7
ip <ip@sigxcpu.com>
2009-09-23 09:34:20 +0000

    + Mtq

---
+++ mtq.ml
@@ -0,0 +1,13 @@
+
+open Control
+
+type 'a t = { mutex : Mutex.t; cond : Condition.t; q : 'a Queue.t; }
+
+let create () = { mutex = Mutex.create (); cond = Condition.create (); q = Queue.create (); }
+
+let put q v = locked q.mutex (fun () -> Queue.push v q.q; Condition.signal q.cond)
+
+let get q = locked q.mutex (fun () ->
+  while Queue.is_empty q.q do Condition.wait q.cond q.mutex done;
+  Queue.pop q.q)
+
---
+++ mtq.mli
@@ -0,0 +1,13 @@
+(** Queue shared between multiple threads *)
+
+type 'a t
+
+(** Create queue *)
+val create : unit -> 'a t
+
+(** Put item into the queue and return immediately *)
+val put : 'a t -> 'a -> unit
+
+(** Get item from the queue (will block while queue is empty) *)
+val get : 'a t -> 'a
+
