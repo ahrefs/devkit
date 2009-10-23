15b1aaa54dc8f397c36844e9a48ff06dc211fc91
ip <ip@sigxcpu.com>
2009-10-23 12:31:18 +0000

    minor

--- logger.ml
+++ logger.ml
@@ -40,14 +40,8 @@ end
 let output_ch ch = 
   fun str -> output_string ch str; flush ch
 
-let time_string_ms f = 
-  let module U = Unix in
-  let t = U.gmtime f in
-  sprintf "%04u-%02u-%02uT%02u:%02u:%09fZ" (1900 + t.U.tm_year) (t.U.tm_mon+1) t.U.tm_mday t.U.tm_hour t.U.tm_min 
-  (mod_float f 60.)
-
 let format_simple l _ s =
-  sprintf "[%s] : %06u:%04u : [%5s] %s\n" (time_string_ms (Unix.gettimeofday ())) (Unix.getpid ()) (Thread.id (Thread.self ())) (level_string l) s
+  sprintf "[%s] : %06u:%04u : [%5s] %s\n" (Time.gmt_string_ms (Unix.gettimeofday ())) (Unix.getpid ()) (Thread.id (Thread.self ())) (level_string l) s
 
 let filter_none _ _ _ = true
 
