bcdc19b3e03db7e8b347a7056857967961886ee8
ip <ip@sigxcpu.com>
2009-10-23 08:30:26 +0000

    default varz - "gc stats"

--- varz.ml
+++ varz.ml
@@ -83,3 +83,30 @@ let restore s =
   let cl = (Marshal.from_string s 0 : (string*string) list) in
   List.iter (fun (k,v) -> ignore (set_control k v)) cl
 
+open Printf
+
+let caml_words f = (* oh ugly *)
+  let f = f *. (float_of_int (Sys.word_size / 8)) in
+  if f < 1024. then sprintf "%uB" (int_of_float f) else
+  if f < 1024. *. 1024. then sprintf "%uKB" (int_of_float (f /. 1024.)) else
+  if f < 1024. *. 1024. *. 1024. then sprintf "%.1fMB" (f /. 1024. /. 1024.) else
+  sprintf "%.1fGB" (f /. 1024. /. 1024. /. 1024.)
+
+let _ = fun_value "gc stats"
+  (fun () ->
+    let st = Gc.quick_stat () in
+    let out = IO.output_string () in
+    IO.printf out "CPU: %.3fs " (Sys.time ());
+    IO.printf out "Heap(max): %s(%s) "
+        (caml_words (float_of_int st.Gc.heap_words)) 
+        (caml_words (float_of_int st.Gc.top_heap_words));
+    IO.printf out "Counters(min,pro,maj): %s %s %s "
+        (caml_words st.Gc.minor_words)
+        (caml_words st.Gc.promoted_words)
+        (caml_words st.Gc.major_words);
+    IO.printf out "Collections(cmp,maj,min): %u %u %u "
+        st.Gc.compactions 
+        st.Gc.major_collections 
+        st.Gc.minor_collections;
+    IO.close_out out)
+
