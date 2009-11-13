38c1bc5345c43f69cffa4e011ea3a73e60f8e244
ip <ip@sigxcpu.com>
2009-11-13 15:01:58 +0000

    Prelude.const

--- prelude.ml
+++ prelude.ml
@@ -6,6 +6,7 @@ let (>>) x f = f x
 external id : 'a -> 'a = "%identity"
 let flip f x y = f y x
 let some x = Some x
+let const x = fun () -> x
 
 let apply2 f = fun (x,y) -> f x, f y
 (* let println = print_endline *)
