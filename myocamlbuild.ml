e35e5139fd63d0452db08d1a79c08d9ebdb95239
ip <ip@sigxcpu.com>
2010-04-30 08:01:30 +0000

    use ocsigen_xhtml

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -58,6 +58,7 @@ dispatch begin function
      C.extern "curl";
      C.extern "fileutils";
      C.extern "ocsigen.xhtml" ~cma:"xhtml";
+     C.extern "ocsigen_xhtml" ~cma:"xhtml"; (* http://bugs.debian.org/579689 *)
      C.extern "event" ~cma:"liboevent";
 
      flag ["ocaml"; "doc"; "use_extLib"] (S[A"-I"; A (C.lib "extlib")]);
