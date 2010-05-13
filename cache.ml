e3c3c9c8f8edbaa95776b042cf9e7df4536c36f8
ip <ip@sigxcpu.com>
2010-05-13 14:06:56 +0000

    minor

--- cache.ml
+++ cache.ml
@@ -107,6 +107,9 @@ struct
   let create () = create 100
   let add t x = match find_option t x with None -> add t x 1 | Some n -> replace t x (n+1)
   let enum t = enum t
+  let show t f = enum t >> 
+    Enum.map (fun (k,v) -> Printf.sprintf "%s: %u" (f k) v) >>
+    Stre.concat " "
 end
 
 module Group : sig
