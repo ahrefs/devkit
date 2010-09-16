a7d77613a57b5a927b71b4f19828cab155393008
ip <ip@sigxcpu.com>
2010-09-16 08:07:39 +0000

    + to_string from_string

--- persist.ml
+++ persist.ml
@@ -1,6 +1,7 @@
 (** Safe marshalling *)
 
 open Control
+open ExtLib
 
 module type Value =
 sig
@@ -24,6 +25,12 @@ let from_channel ch =
   really_input ch s 0 (String.length V.tag);
   if s <> V.tag then raise Error;
   (Marshal.from_channel ch : t)
+
+let to_string ?(flags=[]) x = V.tag ^ Marshal.to_string (x:t) flags
+let from_string s = 
+  let tag = String.slice s ~last:(String.length V.tag) in
+  if tag <> V.tag then raise Error;
+  (Marshal.from_string s (String.length V.tag) : t)
 
 (** FIXME windows *)
 let to_file name ?(mode=0o644) ?(flags=[]) x =
