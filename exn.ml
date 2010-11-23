c5007b8db6c9f42fc956e90bbb74dd376e84f3bd
ip <ip@sigxcpu.com>
2010-11-23 10:16:15 +0000

    Exn describe Pcre.Error too

--- exn.ml
+++ exn.ml
@@ -21,6 +21,17 @@ let to_string = function
   | Unix.Unix_error (e,f,s) -> sprintf "Unix_error %s(%s) %s" f s (Unix.error_message e)
   | Xmlm.Error ((p1,p2),e) -> sprintf "Xmlm.Error((%u,%u),%s)" p1 p2 (Xmlm.error_message e)
   | Curl.CurlException (_,n,s) -> sprintf "Curl.CurlException(%u,%s)" n s
+  | Pcre.Error err -> sprintf "Pcre.Error(%s)"
+    begin match err with
+    | Pcre.Partial -> "Partial"
+    | Pcre.BadPartial -> "BadPartial"
+    | Pcre.BadPattern(m,p) -> sprintf "BadPattern(%s,%i)" m p
+    | Pcre.BadUTF8 -> "BadUTF8"
+    | Pcre.BadUTF8Offset -> "BadUTF8Offset"
+    | Pcre.MatchLimit -> "MatchLimit"
+    | Pcre.RecursionLimit -> "RecursionLimit"
+    | Pcre.InternalError s -> sprintf "InternalError(%s)" s
+    end
   | exn -> Printexc.to_string exn
 
 let str = to_string
