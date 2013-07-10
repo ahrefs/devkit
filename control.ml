d62981b357ca02ce48887daed9050c854518b9e1
Zhenya <lykhovyd@gmail.com>
2013-07-10 16:15:11 +0300

    tweak params

--- control.ml
+++ control.ml
@@ -20,7 +20,7 @@ let with_open_in_txt name = bracket (open_in name) close_in_noerr
 let with_open_out_txt name = bracket (open_out name) close_out_noerr
 let with_open_in_bin name = bracket (open_in_bin name) close_in_noerr
 let with_open_out_bin name = bracket (open_out_bin name) close_out_noerr
-let with_open_out_temp_file ~mode = bracket (Filename.open_temp_file ~mode "dvkt" "tmp") (fun (_,ch) -> close_out_noerr ch)
+let with_open_out_temp_file ?temp_dir ~mode = bracket (Filename.open_temp_file ~mode ?temp_dir "dvkt" "tmp") (fun (_,ch) -> close_out_noerr ch)
 let with_open_out_temp_bin k = with_open_out_temp_file ~mode:[Open_binary] k
 let with_open_out_temp_txt k = with_open_out_temp_file ~mode:[Open_text] k
 
