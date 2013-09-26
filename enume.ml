8816b5d9802d2d594d7de555fc81ecf3a6471a67
Roman Vorobets <roman.vorobets@gmail.com>
2013-09-26 13:28:07 +0800

    Better Enume.find_peek

--- enume.ml
+++ enume.ml
@@ -3,16 +3,15 @@ open Prelude
 exception Exit
 
 (* same as Enum.find, but found element is peeked, but not junked *)
-let find_peek f e =
-  try
-    while true do
-      if not & f & Option.get & Enum.peek e then Enum.junk e else raise Exit
-    done;
-    assert false (* unreachable *)
-  with
-  | Exit -> Option.get & Enum.peek e
-  | Option.No_value -> raise Not_found
-  | exn -> raise exn (* f *)
+let rec find_peek f e =
+  match Enum.peek e with
+  | Some x when f x ->
+    x
+  | None ->
+    raise Not_found
+  | _ ->
+    Enum.junk e;
+    find_peek f e
 
 let list_loop l =
   assert (l <> []);
