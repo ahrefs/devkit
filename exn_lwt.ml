71b39f874b93d2ebfcda0308b3019c76c3f660bc
ip <ip@sigxcpu.com>
2014-03-20 17:00:36 +0800

    Exn_lwt: Exn analogue for Lwt

---
+++ exn_lwt.ml
@@ -0,0 +1,14 @@
+(**
+  Dealing with Lwt exceptions
+*)
+
+open Printf
+
+let catch f x = Lwt.catch (fun () -> Lwt.bind (f x) (fun r -> Lwt.return (Some r))) (fun _ -> Lwt.return None)
+let map f x = Lwt.catch (fun () -> Lwt.bind (f x) (fun r -> Lwt.return (`Ok r))) (fun exn -> Lwt.return (`Exn exn))
+
+let fail ?exn fmt =
+  let fails s = match exn with None -> Lwt.fail (Failure s) | Some exn -> Lwt.fail (Failure (s ^ " : " ^ Exn.to_string exn)) in
+  ksprintf fails fmt
+
+let invalid_arg fmt = ksprintf (fun s -> Lwt.fail (Invalid_argument s)) fmt
