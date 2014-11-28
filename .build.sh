876415fd236bc765c333d68b0ec1b240ef1b2db4
ip <igor@ahrefs.com>
2014-11-28 14:05:17 +0800

    build: opam upgrade

--- .build.sh
+++ .build.sh
@@ -1,9 +1,15 @@
 #! /bin/sh
 set -e -u
 
+export OPAMKEEPBUILDDIR=true
+export OPAMBUILDTEST=true
+export OPAMYES=true
+
 opam update
 opam sw 4.02.1
 eval $(opam config env)
-opam uninstall -y devkit
-opam install -y --deps-only devkit
+opam uninstall devkit
+opam upgrade
+opam install --deps-only devkit
+
 make lib test
