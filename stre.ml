c4d1dd9baf54a683442bed9f1e0e380a4d16b586
ip <igor@ahrefs.com>
2015-06-22 19:48:05 -0700

    modernize Stre

--- stre.ml
+++ stre.ml
@@ -134,31 +134,8 @@ let enum_matches rex s =
 
 let enum_extract rex s = enum_matches rex s |> Enum.map (flip Pcre.get_substring 1)
 
-let erase_dots s =
-  let rec erase = parser
-  | [< ''\''; t >] -> erase t
-  | [< ''0'..'9' as d; t >] -> after_digit d t
-  | [< ''.'; t=dots >] -> [< '' '; erase t >]
-  | [< 'c; t >] -> [< 'c; erase t >]
-  | [< >] -> [< >]
-  and after_digit d1 = parser
-  | [< ''.'; t >] ->
-    begin match Stream.peek t with
-    | Some ('0'..'9') -> [< 'd1; ''.'; erase t >]
-    | _ -> [< 'd1; '' '; erase t >]
-    end
-  | [< t >] -> [< 'd1; erase t >]
-  and dots = parser
-  | [< ''.'; t >] -> dots t
-  | [< t >] -> t
-  in
-  erase (Stream.of_string s)
-
 let string_of_stream s =
   let b = Buffer.create 32 in Stream.iter (Buffer.add_char b) s; Buffer.contents b
-
-let split_words = split by_words $ string_of_stream $ erase_dots
-let split_words s = try split_words s with exn -> Log.self #warn ~exn "split_words \"%s\"" s; [s]
 
 module ASCII = struct
 let is_alpha = function
@@ -170,19 +147,13 @@ let is_digit = function
 let is_alnum c = is_alpha c || is_digit c
 end
 
-let unescaped s =
-  try Scanf.sscanf ("\""^s^"\"") "%S%!" (fun a -> a)  with  _ -> (Exn.fail "Stre.unescaped : Wry input %s" s)
-
 let rev = String.implode $ List.rev $ String.explode
 
-(** deprecated in favor of [common_prefix] *)
-let find_prefix s1 s2 =
+let common_prefix s1 s2 =
   let i = ref 0 in
   let min_len = min (String.length s1) (String.length s2) in
   while !i < min_len && s1.[!i] = s2.[!i] do incr i done;
   !i
-
-let common_prefix = find_prefix
 
 let shorten limit s =
   let limit = max limit 24 in
