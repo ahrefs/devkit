f7450f5a9d02d505782dc851bf5ca2a0e0d9a230
ip <igor@ahrefs.com>
2015-08-26 16:27:36 -0700

    + Persist.Marshal_ext

--- persist.ml
+++ persist.ml
@@ -27,10 +27,14 @@ let from_channel ch =
   (Marshal.from_channel ch : t)
 
 let to_string ?(flags=[]) x = V.tag ^ Marshal.to_string (x:t) flags
-let from_string s =
+
+(** @param also - additional tags allowed (for backward compatibility) *)
+let from_string_ext also s =
   let tag = String.slice s ~last:(String.length V.tag) in
-  if tag <> V.tag then raise Error;
+  if tag <> V.tag && List.for_all ((<>) tag) also then raise Error;
   (Marshal.from_string s (String.length V.tag) : t)
+
+let from_string s = from_string_ext [] s
 
 let to_file_exn name ?mode ?(flags=[]) x =
   Files.save_as name ?mode (fun ch -> to_channel ch ~flags x)
@@ -40,3 +44,20 @@ let from_file name =
 
 end
 
+module type Value_ext =
+sig
+type value
+val tag : string
+val also : string list
+end
+
+module Marshal_ext(V:Value_ext) =
+struct
+let () =
+  List.iter (fun tag -> assert String.(length tag = length V.tag)) V.also
+
+include Marshal(V)
+
+let from_string s = from_string_ext V.also s
+
+end
