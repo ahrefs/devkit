5a315095d7957a409747d24a76f35a8fec7efa1d
ip <igor@ahrefs.com>
2015-12-14 16:57:21 -0800

    HtmlStream: up

--- htmlStream.ml
+++ htmlStream.ml
@@ -30,7 +30,7 @@ let label = String.lowercase
 (** parse stream of characters @return stream of html elements *)
 let rec parse = parser
   | [< ''<'; x = tag; t >] -> [< 'x; parse t >]
-  | [< 'c; x = chars c (neq '<'); t >] -> [< 'Text (Raw.inj x); parse t >]
+  | [< 'c; x = chars c (neq '<'); t >] -> [< 'Text (Raw.inject x); parse t >]
   | [< >] -> [< >]
   and tag = parser
   | [< 'c when is_ident c; name = chars c is_ident; () = skip is_ws; a=tag_attrs [] >] -> Tag (label name,List.rev a)
@@ -43,8 +43,8 @@ let rec parse = parser
   | [< ''>' >] -> a
   | [< 'c when is_ident c; name = chars c is_ident; () = skip is_ws; t >] ->
     begin match try Some (maybe_value t) with EndTag -> None with
-    | Some v -> skip is_ws t; tag_attrs ((label name, Raw.inj v) :: a) t
-    | None -> (label name, Raw.inj "") :: a
+    | Some v -> skip is_ws t; tag_attrs ((label name, Raw.inject v) :: a) t
+    | None -> (label name, Raw.inject "") :: a
     end
   | [< t >] -> skip_till '>' t; a (* skip garbage *)
   and maybe_value = parser
@@ -103,10 +103,10 @@ let show_raw c elem =
   | Tag (name,attrs) ->
     wrapped_output (IO.output_string ()) begin fun out ->
       IO.printf out "<%s" name;
-      List.iter (fun (k,v) -> IO.printf out " %s=%c%s%c" k c (Raw.proj v) c) attrs;
+      List.iter (fun (k,v) -> IO.printf out " %s=%c%s%c" k c (Raw.project v) c) attrs;
       IO.printf out ">"
     end
-  | Text t -> Raw.proj t
+  | Text t -> Raw.project t
   | Close name -> sprintf "</%s>" name
 
 let show_raw' = show_raw '\''
@@ -120,20 +120,20 @@ let dump_debug = Stream.iter (print_endline $ show_raw) $ parse $ Stream.of_stri
 
 let tag name ?(a=[]) = function
   | Tag (name',attrs) when name = name' ->
-    let attrs = lazy (List.map (fun (k,v) -> (k,String.nsplit (Raw.proj v) " ")) attrs) in
+    let attrs = lazy (List.map (fun (k,v) -> (k,String.nsplit (Raw.project v) " ")) attrs) in
     begin try List.for_all (fun (k,v) -> assert (not @@ String.contains v ' '); List.mem v (List.assoc k !!attrs)) a with Not_found -> false end
   | _ -> false
 
 let close name = function Close name' when name = name' -> true | _ -> false
 
 let to_text ?(br=false) ?(strip=false) = function
-  | Tag ("br",_) when br -> Some (Raw.inj "\n")
+  | Tag ("br",_) when br -> Some (Raw.inject "\n")
   | Tag _ -> None
-  | Text x -> Some (if strip then Raw.inj (String.strip (Raw.proj x)) else x)
+  | Text x -> Some (if strip then Raw.inject (String.strip (Raw.project x)) else x)
   | Close _ -> None
 
 (** extract text from the list elements *)
-(* let make_text l = wrapped_outs (fun out -> List.iter (Option.may (IO.nwrite out) $ Option.map Raw.proj $ to_text) l) *)
+(* let make_text l = wrapped_outs (fun out -> List.iter (Option.may (IO.nwrite out) $ Option.map Raw.project $ to_text) l) *)
 let make_text ?br l =
   let fold e =
     let b = Buffer.create 10 in
@@ -141,4 +141,4 @@ let make_text ?br l =
     Buffer.contents b
   in
   List.enum l |> Enum.filter_map (to_text ?br ~strip:true) |>
-  Enum.map Raw.proj |> fold |> Raw.inj
+  Enum.map Raw.project |> fold |> Raw.inject
