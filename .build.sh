52b7e52dd19c8273b726fff367a81c65c033a739
ip <igor@ahrefs.com>
2016-11-21 22:33:17 -0800

    build: use opam upgrade --fixup

--- .build.sh
+++ .build.sh
@@ -9,7 +9,7 @@ opam update
 opam sw 4.02.3
 eval $(opam config env)
 opam uninstall devkit
-opam upgrade
+opam upgrade --fixup
 opam install --deps-only devkit
 
 make BUILDFLAGS="-tag warn_error_A,warn_error_d" distclean lib test
