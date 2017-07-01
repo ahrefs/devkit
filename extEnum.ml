5e15b7d709294c8a755688e44d17d6746f7ddd7d
Bogdan Rybiy <bogdan.rybiy@ahrefs.com>
2017-06-30 18:47:50 -0700

    enum: minor cosmetic

--- extEnum.ml
+++ extEnum.ml
@@ -178,14 +178,14 @@ let count_unique equal e =
   in
   from next
 
-let sub ?(cmp=(=)) e f =
+let sub ?(eq=(=)) e f =
   match peek e with
   | None -> None
   | Some x ->
     let current = f x in
     let next () =
       match peek e with
-      | Some x when cmp (f x) current -> junk e; x
+      | Some x when eq (f x) current -> junk e; x
       | None | Some _ -> raise No_more_elements
     in
     Some (current, from next)
