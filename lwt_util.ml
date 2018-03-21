edeb2347f19145136ded26076cc83a4526f49563
Raman Varabets <raman@ahrefs.com>
2018-03-21 15:49:32 +0800

    + Lwt_util

---
+++ lwt_util.ml
@@ -0,0 +1,46 @@
+
+let log = Log.from "lwt_util"
+
+let with_count nr lwt = incr nr; (lwt)[%lwt.finally decr nr; Lwt.return_unit]
+
+let timely period f =
+  assert (period > 0.);
+  let next = ref (Time.get () +. period) in
+  (fun x -> if Time.get () > !next then (next := Time.get () +. period; f x) else Lwt.return_unit)
+
+let timely_loop' ?(immediate=false) period f =
+  let rec loop () =
+    let%lwt () = try%lwt f () with exn -> log #error ~exn "timely_loop_lwt"; Lwt.return_unit in
+    let%lwt () = Lwt_unix.sleep period in
+    loop ()
+  in
+  let%lwt () = if immediate then Lwt.return_unit else Lwt_unix.sleep period in
+  loop ()
+
+(* run f every period seconds; run immediately if immediate is true; stop when wait thread terminates *)
+let timely_loop ?immediate ?(wait=Daemon.wait_exit ()) period f = Lwt.pick [ wait; timely_loop' ?immediate period f; ]
+
+(* cancel t1 when t2 terminates *)
+let ensure_order t1 t2 = (t2) [%finally Lwt.wrap1 Lwt.cancel t1; ]
+
+(* wait for t to terminate, suppress any exception, and call cleanup () afterwards *)
+let suppress_exn name cleanup t =
+  log #info "%s started" name;
+  let%lwt () =
+    try%lwt
+      let%lwt () = t in
+      log #info "%s done" name;
+      Lwt.return_unit
+    with exn ->
+      log #error ~exn "%s" name;
+      Lwt.return_unit
+  in
+  cleanup ()
+
+let action name f x =
+  log #info "action %s started" name;
+  match%lwt f x with
+  | exception exn -> log #error ~exn "action %s aborted" name; Lwt.fail exn
+  | x -> log #info "action %s done" name; Lwt.return x
+
+let action_do name f = action name f ()
---
+++ lwt_util.mli
@@ -0,0 +1,21 @@
+(** Various utilities for use with Lwt. *)
+
+val with_count : int ref -> 'a Lwt.t -> 'a Lwt.t
+
+val timely : Time.t -> ('a -> unit Lwt.t) -> ('a -> unit Lwt.t)
+
+(** [timely_loop' ?immediate period f] run f every period seconds; run immediately if immediate is true. *)
+val timely_loop' : ?immediate:bool -> Time.t -> (unit -> unit Lwt.t) -> unit Lwt.t
+
+(** [timely_loop' ?immediate ?wait period f] run f every period seconds; run immediately if immediate is true; stop when wait thread terminates. *)
+val timely_loop : ?immediate:bool -> ?wait:unit Lwt.t -> Time.t -> (unit -> unit Lwt.t) -> unit Lwt.t
+
+(** [ensure_order t1 t2] cancel t1 when t2 terminates. *)
+val ensure_order : 'a Lwt.t -> 'b Lwt.t -> 'b Lwt.t
+
+(** [suppress_exn name cleanup t] wait for t to terminate, suppress any exception, and call cleanup () afterwards. *)
+val suppress_exn : string -> (unit -> 'a Lwt.t) -> unit Lwt.t -> 'a Lwt.t
+
+val action : string -> ('a -> 'b Lwt.t) -> 'a -> 'b Lwt.t
+
+val action_do : string -> (unit -> 'a Lwt.t) -> 'a Lwt.t
