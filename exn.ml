72526adbbaa33269bb6b22433dac16119fe55ab5
ip <igor@ahrefs.com>
2018-04-15 00:35:29 +0300

    build: drop xmlm dep

--- exn.ml
+++ exn.ml
@@ -14,7 +14,6 @@ let map f x = try `Ok (f x) with exn -> `Exn exn
 
 let to_string = function
   | Unix.Unix_error (e,f,s) -> sprintf "Unix_error %s(%s) %s" f s (Unix.error_message e)
-  | Xmlm.Error ((p1,p2),e) -> sprintf "Xmlm.Error((%u,%u),%s)" p1 p2 (Xmlm.error_message e)
   | Curl.CurlException (_,n,s) -> sprintf "Curl.CurlException(%u,%s)" n s
   | Pcre.Error err -> sprintf "Pcre.Error(%s)"
     begin match err with
