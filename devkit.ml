e9e40f31639051ccab7129c02697584940962be0
Raman Varabets <raman@ahrefs.com>
2019-01-03 10:06:01 +0800

    don't include test modules into library

--- devkit.ml
+++ devkit.ml
@@ -1,9 +1,3 @@
 
-module Devkit_core : sig
-  include module type of Devkit_core with module Test := Devkit_core.Test
-end = struct
-  include Devkit_core
-end
-
 include Devkit_core
 include Prelude
