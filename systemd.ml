c390fad46076cbfe7d415aac3de9ad174c70a5bd
Louis Roché <louis.roche@ahrefs.com>
2019-03-25 04:16:43 +0000

    add systemd utils

---
+++ systemd.ml
@@ -0,0 +1,55 @@
+open ExtLib
+
+let log = Log.from "systemd"
+
+module Daemon = struct
+
+  (* https://github.com/systemd/systemd/blob/cb3108669d623afe58a36077e36ae4c66ff7d1c3/src/systemd/sd-daemon.h#L56 *)
+  (* The first passed file descriptor is fd 3. *)
+  let sd_listen_fds_start = 3
+
+  let booted =
+    (* https://github.com/systemd/systemd/blob/cb31086/src/libsystemd/sd-daemon/sd-daemon.c#L607 *)
+    try
+      Unix.access "/run/systemd/system/" Unix.[F_OK];
+      true
+    with Unix.Unix_error _ ->
+      false
+
+  let listen_pid =
+    Option.map int_of_string (Sys.getenv_opt "LISTEN_PID")
+
+  let listen_fds () : Unix.file_descr list =
+    (* https://github.com/systemd/systemd/blob/cb31086/src/libsystemd/sd-daemon/sd-daemon.c#L42-L90 *)
+    match booted with
+    | false ->
+      log#debug "listen_fds: not booted with systemd";
+      []
+    | true ->
+    match listen_pid with
+    | None ->
+      log#debug "listen_fds: no LISTEN_PID";
+      []
+    | Some listen_pid ->
+    let self_pid = Unix.getpid () in
+    match listen_pid = self_pid with
+    | false ->
+      log#warn "listen_fds: LISTEN_PID %d and process pid %d are not equal, ignoring" listen_pid self_pid;
+      []
+    | true ->
+    let listen_fds = Option.map int_of_string (Sys.getenv_opt "LISTEN_FDS") in
+    match listen_fds with
+    | None ->
+      log#warn "listen_fds: LISTEN_PID, but no LISTEN_FDS";
+      []
+    | Some n when n <= 0 ->
+      log#warn "listen_fds: LISTEN_FDS %d is not positive" n;
+      []
+    | Some n ->
+      let fds = List.init n (fun x -> ExtUnixAll.file_descr_of_int (x + sd_listen_fds_start)) in
+      List.iter Unix.set_close_on_exec fds;
+      fds
+
+  let listen_fds_lwt () =
+    List.map Lwt_unix.of_unix_file_descr (listen_fds ())
+end
---
+++ systemd.mli
@@ -0,0 +1,29 @@
+(**
+   Misc utils for systemd.
+*)
+
+(** Subset of [sd-daemon] in ocaml.
+
+    The following functionalities are provided:
+    - File descriptor passing for socket-based activation
+    - Detection of systemd boots
+*)
+module Daemon : sig
+
+  (** [true] if the system was booted with systemd. *)
+  val booted : bool
+
+  (**
+     Returns file descriptors that have been passed by systemd.
+
+     This function call ensures that the [FD_CLOEXEC] flag is set for
+     the passed file descriptors, to make sure they are not passed on
+     to child processes. If [FD_CLOEXEC] shall not be set, the caller
+     needs to unset it after this call for all file descriptors that
+     are used.
+  *)
+  val listen_fds : unit -> Unix.file_descr list
+
+  (** Same as {!listen_fds} but return lwt file descriptors. *)
+  val listen_fds_lwt : unit -> Lwt_unix.file_descr list
+end
