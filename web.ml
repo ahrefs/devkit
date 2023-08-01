be0cba8d1db9e7f1d32b1e12dbb6c57c619ef185
Elliott Cable <elliott.cable@ahrefs.com>
2023-08-01 22:14:57 +0000

    web: Defunctorize and use new Trace/ambient-context

--- web.ml
+++ web.ml
@@ -6,8 +6,6 @@ open Printf
 open Prelude
 open Control
 
-module Otel = Opentelemetry
-
 let log = Log.self
 
 (** percent-encode (convert space into %20) *)
@@ -121,21 +119,6 @@ module type CURL = sig
   val perform : Curl.t -> Curl.curlCode t
 end
 
-module type TRACING = sig
-  type 'a io
-  type span_id
-
-  val get_traceparent : unit -> string option
-
-  val enabled : unit -> bool
-
-  val with_span :
-    __FILE__ : string ->
-    __LINE__ : int ->
-    string ->
-    (span_id -> 'a io) -> 'a io
-end
-
 type ('body,'ret) http_request_ =
   ?ua:string ->
   ?timeout:int ->
@@ -185,12 +168,7 @@ let simple_result ?(is_ok=(fun code -> code / 100 = 2)) ?verbose = function
 
 let nr_http = ref 0
 
-module Http
-  (IO : IO_TYPE)
-  (Curl_IO : CURL with type 'a t = 'a IO.t)
-  (Tracing : TRACING with type 'a io = 'a IO.t)
-: HTTP with type 'a IO.t = 'a IO.t =
-struct
+module Http (IO : IO_TYPE) (Curl_IO : CURL with type 'a t = 'a IO.t) : HTTP with type 'a IO.t = 'a IO.t = struct
 
   module IO = IO
 
@@ -270,14 +248,16 @@ struct
   (* NOTE don't forget to set http_1_0=true when sending requests to a Httpev-based server *)
   (* Don't use curl_setheaders when using ?headers option *)
   let http_request' ?ua ?timeout ?(verbose=false) ?(setup=ignore) ?timer ?max_size ?(http_1_0=false) ?headers ?body (action:http_action) url =
+    let module Otel = Opentelemetry in
     let open Curl in
     let action_name = string_of_http_action action in
-    Tracing.with_span ~__FILE__ ~__LINE__ action_name @@ fun _span_id ->
+    Trace.with_span ~__FUNCTION__ ~__FILE__ ~__LINE__ action_name @@ fun _span_id ->
 
-    let headers = match Tracing.get_traceparent () with
+    let headers = match Otel.Scope.get_ambient_scope () with
     | None -> headers
-    | Some tp ->
-      let tp_header = Otel.Trace_context.Traceparent.name ^ ": " ^ tp in
+    | Some Opentelemetry.Scope.{ trace_id; span_id; _ } ->
+      let tp_value = Otel.Trace_context.Traceparent.to_value ~trace_id ~parent_id:span_id () in
+      let tp_header = Otel.Trace_context.Traceparent.name ^ ": " ^ tp_value in
       Some (tp_header :: (Option.default [] headers))
     in
 
@@ -380,56 +360,8 @@ module Curl_lwt_for_http = struct
   include Curl_lwt
 end
 
-
-module Tracing_stack = struct
-  module TLS = Otel.Thread_local
-
-  type 'a io = 'a
-
-  type span_id = Trace.span
-
-  let _traceparent_key : string TLS.t = TLS.create ()
-
-  let get_traceparent () =
-    TLS.get _traceparent_key
-
-  let with_traceparent s cb =
-    TLS.with_ _traceparent_key s cb
-
-  let enabled = Trace.enabled
-
-  let with_span ~__FILE__ ~__LINE__ name cb =
-    let span_id = Trace.enter_span ~__FILE__ ~__LINE__ name in
-    let rv = cb span_id in
-    Trace.exit_span span_id;
-    rv
-end
-
-module Tracing_lwt = struct
-  type 'a io = 'a Lwt.t
-
-  type span_id = Otel.Span_id.t
-
-  let _traceparent_key : string Lwt.key = Lwt.new_key ()
-
-  let get_traceparent () =
-    Lwt.get _traceparent_key
-
-  let with_traceparent s cb =
-    Lwt.with_value _traceparent_key s cb
-
-  let enabled = Otel.Collector.has_backend
-
-  let with_span ~__FILE__ ~__LINE__ name cb =
-    log#warn "Attempt to trace at %s:%i; Http_lwt currently doesn't support async scopes during tracing; pending imandra-ai/ocaml-opentelemetry#32. Replace Tracing_lwt with your own scope-tracking implementation." __FILE__ __LINE__;
-    let attrs = [ "file", `String __FILE__; "line", `Int __LINE__ ] in
-    Opentelemetry_lwt.Trace.with_
-      ~attrs ~kind:Otel.Proto.Trace.Span_kind_client name
-      (fun scope -> cb scope.Otel.Scope.span_id)
-end
-
-module Http_blocking = Http(IO_blocking)(Curl_blocking)(Tracing_stack)
-module Http_lwt = Http(IO_lwt)(Curl_lwt_for_http)(Tracing_lwt)
+module Http_blocking = Http(IO_blocking)(Curl_blocking)
+module Http_lwt = Http(IO_lwt)(Curl_lwt_for_http)
 
 let with_curl = Http_blocking.with_curl
 let with_curl_cache = Http_blocking.with_curl_cache
