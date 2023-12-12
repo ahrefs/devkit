920baf1f1a398535f7c672cb68fc35b13ed157e2
Javier Chavarri <javier.chavarri@gmail.com>
2023-12-12 14:20:43 +0000

    simplify moar

--- ocamlnet_lite/netaux.ml
+++ ocamlnet_lite/netaux.ml
@@ -31,23 +31,4 @@ module ArrayAux = struct
   let int_blit src srcpos dest destpos len = 
     !int_blit_ref src srcpos dest destpos len
 
-  let int_series_ref =
-    ref
-      (fun src srcpos dst dstpos len n ->
-	 if (len < 0 || srcpos < 0 || dstpos < 0 ||
-	     srcpos+len > Array.length src ||
-	     dstpos+len > Array.length dst)
-	 then
-	   invalid_arg "Netaux.ArrayAux.int_series";
-
-	 let s = ref n in
-	 for i = 0 to len-1 do
-	   Array.unsafe_set dst (dstpos+i) !s;
-	   s := !s + Array.unsafe_get src (srcpos+i)
-	 done
-      )
-
-  let int_series src srcpos dst dstpos len n =
-    !int_series_ref src srcpos dst dstpos len n
-
 end
