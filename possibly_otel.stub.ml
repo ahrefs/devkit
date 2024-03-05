052f785c398d4c549c290819e7cd38870f8eddbe
Corentin Leruth <corentin.leruth@gmail.com>
2024-03-05 15:22:44 +0100

    use function from Trace_core directly

--- possibly_otel.stub.ml
+++ possibly_otel.stub.ml
@@ -7,5 +7,3 @@ end
 
 let enter_manual_span ~__FUNCTION__ ~__FILE__ ~__LINE__ ?data name =
   Trace_core.enter_manual_toplevel_span ~__FUNCTION__ ~__FILE__ ~__LINE__ ?data name
-
-let add_data_to_manual_span span data = Trace_core.add_data_to_manual_span span data
