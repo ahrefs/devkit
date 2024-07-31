38c565fe7cca85957e86956691380312ace7fe63
Corentin Leruth <corentin.leruth@gmail.com>
2024-07-31 07:36:11 +0200

    exn: preserve backtraces

--- exn_lwt.ml
+++ exn_lwt.ml
@@ -7,8 +7,6 @@ open Printf
 let catch f x = Lwt.try_bind (fun () -> f x) Lwt.return_some (fun _exn -> Lwt.return_none)
 let map f x = Lwt.try_bind (fun () -> f x) (fun r -> Lwt.return (`Ok r)) (fun exn -> Lwt.return (`Exn exn))
 
-let fail ?exn fmt =
-  let fails s = Lwt.fail_with @@ match exn with None -> s | Some exn -> s ^ " : " ^ Exn.to_string exn in
-  ksprintf fails fmt
+let fail = Exn.fail
 
 let invalid_arg fmt = ksprintf Lwt.fail_invalid_arg fmt
