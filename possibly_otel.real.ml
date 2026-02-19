a1887c0684c79787b4bf035ec49b9e7e79bf3e99
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-02-19 15:49:23 -0500

    update for latest trace, ambient-context, and opentelemetry

--- possibly_otel.real.ml
+++ possibly_otel.real.ml
@@ -1,29 +1,26 @@
 open Opentelemetry
 
-let[@inline]  (let*) o f = Option.map f o
+let[@inline] (let*) o f = Option.map f o
 
-type Trace_core.span +=
-  | Span_otel of Scope.t
+open  Opentelemetry_trace.Extensions
 
 module Traceparent = struct
   let name = Trace_context.Traceparent.name
 
   let get_ambient ?explicit_span () =
-    let* Scope.{ trace_id; span_id; _ } = Scope.get_ambient_scope () in
-    let span_id = match explicit_span with
-      | Some {Trace_core.span; _} -> Opentelemetry_trace.Internal.otel_of_otrace span
-      | None -> span_id 
-    in
-    Trace_context.Traceparent.to_value ~trace_id ~parent_id:span_id ()
+    match explicit_span with
+    | Some (Span_otel sp) ->
+      let tp =
+        Trace_context.Traceparent.to_value ~trace_id:(Trace_id.of_bytes sp.trace_id)
+        ~parent_id:(Span_id.of_bytes sp.span_id) () in
+      Some tp
+    | _ ->
+      let* sp = Ambient_span.get () in
+      Trace_context.Traceparent.to_value
+        ~trace_id:(Trace_id.of_bytes sp.trace_id)
+        ~parent_id:(Span_id.of_bytes sp.span_id) ()
 end
 
 let enter_manual_span ~__FUNCTION__ ~__FILE__ ~__LINE__ ?data name =
-    match Scope.get_ambient_scope () with
-    | None ->
-      Trace_core.enter_manual_toplevel_span ~__FUNCTION__ ~__FILE__ ~__LINE__ ?data name
-    | Some Scope.{ span_id; _ } ->
-      let otrace_espan = Trace_core.{
-        span = Opentelemetry_trace.Internal.otrace_of_otel span_id;
-        meta = Trace_core.Meta_map.empty
-      } in
-      Trace_core.enter_manual_sub_span ~parent:otrace_espan ~__FUNCTION__ ~__FILE__ ~__LINE__ ?data name
+  let parent = Ambient_span.get () |> Option.map (fun sp -> Span_otel sp) in
+  Trace_core.enter_span ~parent ~__FUNCTION__ ~__FILE__ ~__LINE__ ?data name
